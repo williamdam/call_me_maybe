7d59c15b02a5e25550b034d770b7e7a137294f31
William Dam <damw@oregonstate.edu>
2021-02-14 19:50:01 -0800

    Comments

--- lib/predictor.dart
+++ lib/predictor.dart
@@ -1,29 +1,53 @@
+//////////////////////////////////////////////////////////////////////
+// Author: William Dam
+// Date: 02-14-2021
+// Description: Predictor class is the view for the question page
+//////////////////////////////////////////////////////////////////////
+
 import 'package:flutter/material.dart';
 import 'models/fortune_teller.dart';
 
+//////////////////////////////////////////////////////////////////////
+// Description: Predictor class returns widget state
+//////////////////////////////////////////////////////////////////////
 class Predictor extends StatefulWidget {
   @override
   _PredictorState createState() => _PredictorState();
 }
 
+//////////////////////////////////////////////////////////////////////
+// Description: Returns widget with new state onTap.  Calls on
+// FortuneTeller class to generate new fortune string, using
+// getAnswer method.
+//////////////////////////////////////////////////////////////////////
 class _PredictorState extends State<Predictor> {
+
+  String fortune = '';
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: predictions(context)
+    return GestureDetector(
+      onTap: () { setState( () { fortune = FortuneTeller().getAnswer();});},
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: predictions(context, fortune)
+      ),
     );
   }
 }
 
-List<Widget> predictions(BuildContext context) {
+//////////////////////////////////////////////////////////////////////
+// Description: List of Text widgets to display fortune teller
+// interface.
+//////////////////////////////////////////////////////////////////////
+List<Widget> predictions(BuildContext context, String fortune) {
   return [
     Text('Call Me...Maybe?', style: Theme.of(context).textTheme.headline4),
     SizedBox(height: 50),
     Text('Ask a question...tap for the answer.', style: TextStyle(fontSize: 20.0)),
     SizedBox(height: 50),
-    Text(FortuneTeller().getAnswer(), style: TextStyle(color: Colors.blue, fontSize: 30, fontWeight: FontWeight.bold)),
+    Text(fortune, style: TextStyle(color: Colors.blue, fontSize: 24, fontWeight: FontWeight.bold)),
     SizedBox(height: 50)
   ]; 
 } 
